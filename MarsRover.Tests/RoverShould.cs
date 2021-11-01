0b5ffa1f440c1c4729f4f4e2d7df0b1eddae4e01
oyont <onur.yont@gmail.com>
2021-11-01 16:11:34 +0300

    added rover class

--- MarsRover.Tests/RoverShould.cs
+++ MarsRover.Tests/RoverShould.cs
@@ -1,18 +1,36 @@
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace MarsRover.Tests
 {
     public class RoverShould
     {
-        [SetUp]
-        public void Setup()
+       [Test]
+        public void Initialize()
         {
+            // arrange
+            var rover = new Rover(0,0,'N');
+
+            rover.ToString().Should().Be("0 0 N");
+        }
+    }
+
+    public class Rover
+    {
+        public int X { get; }
+        public int Y { get; }
+        public char Direction { get; }
+
+        public Rover(int x, int y, char direction)
+        {
+            X = x;
+            Y = y;
+            Direction = direction;
         }
 
-        [Test]
-        public void Test1()
+        public override string ToString()
         {
-            Assert.Pass();
+            return $"{X} {Y} {Direction}";
         }
     }
 }
