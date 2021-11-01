383aa8c6a58feaa603bbb7a76457c60012bbc915
oyont <onur.yont@gmail.com>
2021-11-01 15:54:23 +0300

    First initialize

---
+++ MarsRover.Tests/RoverShould.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace MarsRover.Tests
+{
+    public class RoverShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
